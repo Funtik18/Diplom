7b018d9bfe4f6fcc6b3975a80fd1093d98167743
Funtik18 <sir.funtik18@yandex.ru>
2020-05-11 18:13:37 +0700

    add hex metric

---
+++ TurtleStart/Assets/Scripts/Hex/IHex/Cell.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cell : MonoBehaviour, IHex {
+	
+	public event Action<Cell> OnMouseEnterEvent;
+	public event Action<Cell> OnMouseDownEvent;
+	public event Action<Cell> OnMouseUpAsButtonEvent;
+	public event Action<Cell> OnMouseUpEvent;
+	public event Action<Cell> OnMouseExitEvent;
+
+	private void OnMouseEnter() {
+		if(OnMouseEnterEvent != null)
+			OnMouseEnterEvent(this);
+	}
+	private void OnMouseDown() {
+		if(OnMouseDownEvent != null)
+			OnMouseDownEvent(this);
+	}
+	private void OnMouseUpAsButton() {
+		if(OnMouseUpAsButtonEvent != null)
+			OnMouseUpAsButtonEvent(this);
+	}
+	private void OnMouseUp() {
+		if(OnMouseUpEvent != null)
+			OnMouseUpEvent(this);
+	}
+	private void OnMouseExit() {
+		if(OnMouseExitEvent != null)
+			OnMouseExitEvent(this);
+	}
+	[SerializeField]
+	public HexPoint coordinates;
+	
+	public Material rootMaterial {
+		get { return GetComponent<Renderer>().material; }
+		set { GetComponent<Renderer>().material = value; }
+	}
+}
---
+++ TurtleStart/Assets/Scripts/Hex/IHex/IHex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public interface IHex {
+
+	event Action<Cell> OnMouseEnterEvent;
+	event Action<Cell> OnMouseDownEvent;
+	event Action<Cell> OnMouseUpAsButtonEvent;
+	event Action<Cell> OnMouseUpEvent;
+	event Action<Cell> OnMouseExitEvent;
+
+	Material rootMaterial { get; set; }
+}
